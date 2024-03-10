66d97d8d65b8000c285fcb0547c3bdd712d369f3
trzmielewskiR <eryoop@gmail.com>
2024-03-10 20:16:50 +0100

    rewritten main as class - changed drawing gt and reloading map, Agent refactored and added movement to Agent

--- agent.py
+++ agent.py
@@ -1,13 +1,29 @@
 import pygame
 
 class Agent(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x:int, y:int, img:str):
         super().__init__()
 
-        self.images = []
-        self.image = pygame.image.load("resources/textures/trashmaster_blu.png")
-        self.image = pygame.transform.scale(self.image, (16, 16))
-        self.images.append(self.image)
+        self.img = img
+        self.width = x
+        self.height = y
+
+        self.x = 0
+        self.y = 0
+
+        self.image = pygame.image.load(img) 
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
         self.rect = self.image.get_rect()
 
-        gt_list = pygame.sprite.Group()
+
+    def move(self, key: pygame.key, velocity: int):
+        if key == pygame.K_LEFT:
+            self.x -= velocity
+        if key == pygame.K_RIGHT:
+            self.x += velocity
+        if key == pygame.K_UP:
+            self.y -= velocity
+        if key == pygame.K_DOWN:
+            self.y += velocity
+        
+        return (self.x, self.y)
--- main.py
+++ main.py
@@ -3,29 +3,43 @@ import pygame
 from map import preparedMap
 from agent import Agent
 
-#config
-SCREEN_SIZE: tuple[int, int] = (512, 512)
-BACKGROUND_COLOR: pygame.Color = pygame.Color('#ffffff')
 
-if __name__ == '__main__':
+class AutoGtGame():
+    def __init__(self):
+        self.SCREEN_SIZE = (512, 512)
+        self.BACKGROUND_COLOR = pygame.Color('#ffffff')
 
-    pygame.init()
+        pygame.init()
+        pygame.display.set_caption("autoGT")
 
-    pygame.display.set_caption("autoGT")
+        self.screen = pygame.display.set_mode(size=self.SCREEN_SIZE)
+        self.screen.fill(pygame.Color(self.BACKGROUND_COLOR))
 
-    screen = pygame.display.set_mode(size=SCREEN_SIZE)
-    screen.fill(pygame.Color(BACKGROUND_COLOR))
+        self.map = preparedMap(self.SCREEN_SIZE)
+        self.screen.blit(self.map, (0, 0))
+        
+    def update_window(self):
+        pygame.display.update()
 
-    map = preparedMap(SCREEN_SIZE)
-    
-    screen.blit(map, (0, 0))
 
-    agent = Agent()
-    agent_list = pygame.sprite.Group()
-    agent_list.add(agent)
-    agent_list.draw(screen)
+    def draw_GargabeTruck(self, agent: Agent, position: tuple[int, int]):
+        # pos -> (x, y)
+        self.screen.blit(agent.image, position)
 
-    pygame.display.update()
+
+    def reload_map(self):
+        self.screen.fill(pygame.Color(self.BACKGROUND_COLOR))
+        self.screen.blit(self.map, (0, 0))
+
+
+def main():
+    game = AutoGtGame()
+    game.update_window()
+
+    agent = Agent(16, 16, "resources\\textures\\trashmaster_blu.png")
+    game.draw_GargabeTruck(agent, (0, 0))
+
+    game.update_window()
 
     running = True
 
@@ -33,10 +47,15 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                game.reload_map()
+                game.draw_GargabeTruck(
+                    agent,
+                    agent.move(event.key, 16))
+                game.update_window()
 
     pygame.quit()
 
 
-def AutoGtGame():
-    def __init__(self):
-        pass
+if __name__ == "__main__":
+    main()
